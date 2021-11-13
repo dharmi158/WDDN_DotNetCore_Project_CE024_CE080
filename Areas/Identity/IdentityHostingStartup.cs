fcd0c73ddb4c40daa07b28d95cb919b6a5545ae9
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2021-11-13 12:52:06 +0530

    dot net core project

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SignalRchatApp.Areas.Identity.Data;
+using SignalRchatApp.Data;
+
+[assembly: HostingStartup(typeof(SignalRchatApp.Areas.Identity.IdentityHostingStartup))]
+namespace SignalRchatApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SignalRchatDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SignalRchatDbContextConnection")));
+
+                services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<SignalRchatDbContext>();
+            });
+        }
+    }
+}
